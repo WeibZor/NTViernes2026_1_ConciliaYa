d605bba0992d31f44a5866566afef1c35fa01a0a
Shioran <andresmars45@gmail.com>
2026-04-17 20:05:03 -0500

    se crean cambios

---
+++ Proyecto Integrador/notebook/Limpieza_EstadoConflicto.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def limpiar_simulacion(data_frame):
+    data_frame_limpio=data_frame.copy()
--- Proyecto Integrador/utils/simulacion_estadoConflicto.py
+++ Proyecto Integrador/utils/simulacion_estadoConflicto.py
@@ -20,5 +20,15 @@ def generar_estadoConflictos(num_estadoConflictos):
             "estado":rd.choice(listaActivo),
             "descripcion":rd.choice(listaDescripcion)
         }
+        probabilidadError=rd.random()
+        if(probabilidadError<0.2):
+            estadoConflicto["id"]=None
+        elif(probabilidadError<0.4):
+            estadoConflicto["nombre"]=rd.choice(["MedioCerrado","MedioAbierto"])
+        elif(probabilidadError<0.5):
+            estadoConflicto=rd.choice([1,1000,2])
+        elif(probabilidadError<0.8):
+            estadoConflicto["descripcion"]=" "+estadoConflicto["descripcion"].upper()
+            
         estadosClonfictos.append(estadoConflicto)
     return estadosClonfictos
