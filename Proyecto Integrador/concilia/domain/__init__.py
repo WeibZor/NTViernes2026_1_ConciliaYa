6123ed8c9542fc244842f1383eb37bbd36e12d61
WeibZor <ivandisimo@gmail.com>
2026-05-17 16:07:11 -0500

    chore(domain): crear paquete base concilia.domain y paquete técnico entities

---
+++ Proyecto Integrador/concilia/domain/__init__.py
@@ -0,0 +1,5 @@
+﻿# Este paquete agrupa las entidades del dominio en concilia/domain/entities.
+# Las entidades incluyen conflicto, estado_conflicto, mediacion, perfil,
+# tipoconflicto y usuario.
+
+__all__ = ["entities"]
