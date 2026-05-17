d3e7ec5dd383d919391dc99f38a6b98c2bfde654
WeibZor <ivandisimo@gmail.com>
2026-05-17 16:07:45 -0500

    chore(imports): actualizar imports en mainmomento2.py a concilia.domain.entities

--- Proyecto Integrador/concilia/mains/mainmomento2.py
+++ Proyecto Integrador/concilia/mains/mainmomento2.py
@@ -11,29 +11,29 @@ import pandas as pd
 import uvicorn
 
 # Importaciones de módulos de EstadoConflicto
-from concilia.domain.notebook.HU_6_Limpieza_EstadoConflicto import limpiar_estadosConflictos
-from concilia.domain.notebook.HU_7_Descripción_exploratoria_EstadoConflicto import descripcion_estado_conflicto
-from concilia.domain.utils.HU_8_Simulación_y_exportación_EstadoConflicto import generar_estadoConflictos
+from concilia.domain.entities.estado_conflicto.HU_6_Limpieza_EstadoConflicto import limpiar_estadosConflictos
+from concilia.domain.entities.estado_conflicto.HU_7_Descripción_exploratoria_EstadoConflicto import descripcion_estado_conflicto
+from concilia.domain.entities.estado_conflicto.HU_8_Simulación_y_exportación_EstadoConflicto import generar_estadoConflictos
 
 # Importaciones de módulos de Perfil
-from concilia.domain.notebook.HU16_limpiezaperfil import limpiar_perfil
-from concilia.domain.notebook.HU17_descripcion_perfil import descripcion_perfil
-from concilia.domain.utils.simulacionperfil import generarPerfil
+from concilia.domain.entities.perfil.HU16_limpiezaperfil import limpiar_perfil
+from concilia.domain.entities.perfil.HU17_descripcion_perfil import descripcion_perfil
+from concilia.domain.entities.perfil.simulacionperfil import generarPerfil
 
 # Importaciones de módulos de TipoConflicto
-from concilia.domain.tipoconflicto.HU_21_Limpieza_TipoConflicto import limpiar_tipos_conflicto
-from concilia.domain.tipoconflicto.HU_22_Descripcion_TipoConflicto import descripcion_tipos_conflicto
-from concilia.domain.tipoconflicto.HU_23_Simulacion_TipoConflicto import simular_y_exportar_tipos_conflicto
+from concilia.domain.entities.tipoconflicto.HU_21_Limpieza_TipoConflicto import limpiar_tipos_conflicto
+from concilia.domain.entities.tipoconflicto.HU_22_Descripcion_TipoConflicto import descripcion_tipos_conflicto
+from concilia.domain.entities.tipoconflicto.HU_23_Simulacion_TipoConflicto import simular_y_exportar_tipos_conflicto
 
 # Importaciones de módulos de Usuario
-from concilia.domain.usuario.HU_26_Limpieza_Usuario import limpiar_usuarios
-from concilia.domain.usuario.HU_27_Descripcion_Usuario import descripcion_usuarios
-from concilia.domain.usuario.HU_28_Simulacion_Usuario import simular_y_exportar_usuarios
+from concilia.domain.entities.usuario.HU_26_Limpieza_Usuario import limpiar_usuarios
+from concilia.domain.entities.usuario.HU_27_Descripcion_Usuario import descripcion_usuarios
+from concilia.domain.entities.usuario.HU_28_Simulacion_Usuario import simular_y_exportar_usuarios
 
 # Importaciones de módulos de Conflicto
-from concilia.domain.conflicto.HU_01_Limpieza_Conflicto import limpiar_conflictos
-from concilia.domain.conflicto.HU_02_Descripcion_Conflicto import descripcion_conflictos
-from concilia.domain.conflicto.conflicto_data import generar_datos_conflicto
+from concilia.domain.entities.conflicto.HU_01_Limpieza_Conflicto import limpiar_conflictos
+from concilia.domain.entities.conflicto.HU_02_Descripcion_Conflicto import descripcion_conflictos
+from concilia.domain.entities.conflicto.conflicto_data import generar_datos_conflicto
 
 # Configuración de FastAPI
 app = FastAPI(
